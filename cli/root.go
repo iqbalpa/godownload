2fad8845f00055e637f5287d66f785e4ddabde75
iqbalpa <iqbalpahlevi07@gmail.com>
2025-07-30 06:44:28 +0700

    feat: cli

---
+++ cli/root.go
@@ -0,0 +1,71 @@
+package cli
+
+import (
+	"fmt"
+	"godownload/internal"
+	"sync"
+
+	"github.com/spf13/cobra"
+)
+
+// Services
+var (
+	wg sync.WaitGroup
+)
+
+// Flags
+var (
+	url       string
+	output    string
+	numChunks int
+)
+
+// CLI Commands
+var (
+	rootCmd = &cobra.Command{
+		Use:   "godownload",
+		Short: "Concurrent downloader",
+		Long:  "Godownload is a local concurrent downloader",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Godownload -- Concurrent downloader v0.1.0")
+		},
+	}
+
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of Godownload",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Godownload v0.1.0")
+		},
+	}
+
+	downloadCmd = &cobra.Command{
+		Use:   "download",
+		Short: "Download the file in the given link",
+		Run: func(cmd *cobra.Command, args []string) {
+			size, _ := internal.GetFileMetadata(url)
+			chunks := internal.CreateChunks(size, numChunks)
+			for _, c := range chunks {
+				wg.Add(1)
+				go internal.DownloadChunk(url, c, &wg)
+			}
+			wg.Wait()
+			fmt.Println("Downloaded successfully!")
+			internal.MergeChunks(chunks, output)
+		},
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	downloadCmd.PersistentFlags().StringVarP(&url, "url", "u", "", "the url you want to download")
+	downloadCmd.PersistentFlags().StringVarP(&output, "output", "o", "", "output file name")
+	downloadCmd.PersistentFlags().IntVarP(&numChunks, "chunk", "c", 20, "the number fo goroutines")
+
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(downloadCmd)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"godownload/internal"
-	"sync"
+	"godownload/cli"
 )
 
 func main() {
-	fmt.Println("helo")
-
-	var wg sync.WaitGroup
-
-	url := "https://www.sample-videos.com/video321/mp4/720/big_buck_bunny_720p_30mb.mp4"
-	size, _ := internal.GetFileMetadata(url)
-	chunks := internal.CreateChunks(size, 20)
-
-	for _, c := range chunks {
-		wg.Add(1)
-		go internal.DownloadChunk(url, c, &wg)
-	}
-
-	wg.Wait()
-	fmt.Println("Downloaded successfully!")
-
-	internal.MergeChunks(chunks, "result.mp4")
+	cli.Execute()
 }
